c5cef1a151ca88ad34435a0dc2cb334974d67831
TooTopDope <s.cherednichenko.s@gmail.com>
2022-01-22 13:35:38 +0300

    4.1. Аргументы [#295521 #251014] delete comments in calss Fit

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -3,18 +3,18 @@ package ru.job4j.calculator;
 public class Fit {
 
     public static double manWeight(short height) {
-        double rsl = (height - 100) * 1.15;   //вес мужчины
+        double rsl = (height - 100) * 1.15;
         return rsl;
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;  //вес женщины
+        double rsl = (height - 110) * 1.15;
         return rsl;
     }
 
     public static void main(String[] args) {
-        short manHeight = 190; // рост мужчины
-        short womanHeight = 180; // рост женщины
+        short manHeight = 190;
+        short womanHeight = 180;
         double man = Fit.manWeight(manHeight);
         double woman = Fit.womanWeight(womanHeight);
         System.out.println("Man 190 is " + man);
