ff11940b093e1bb4a81adb8f26102bbbfa978b6a
TooTopDope <s.cherednichenko.s@gmail.com>
2022-02-02 02:14:56 +0300

    3. Тесты для определения расстояния между точками. [#159459 #133770] correcting class Point in last SOUT

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -11,7 +11,7 @@ public class Point {
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         double result1 = Point.distance(1, 1, 1, 1);
-        double result2 = Point.distance(-2, -2, 2, 5);
+        double result2 = Point.distance(-2, -2, -2, -5);
         System.out.println("result (0, 0) to (2, 0) " + " = " + result);
         System.out.println("result (1, 1) to (1, 1) " + " = " + result1);
         System.out.println("result (-2, -2) to (-2, -5) " + " = " + result2);
