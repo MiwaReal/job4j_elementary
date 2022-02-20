c5e2cf1d45349196f40ba5ee1734e508e146ff8a
TooTopDope <s.cherednichenko.s@gmail.com>
2022-02-20 22:40:54 +0300

    6.0.1.1. Размер массива [#333582] added println for every array from ArrayDefinition.

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,10 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("Размер массива типа short равен: " + ages.length);
         String[] surnames = new String[100500];
+        System.out.println("Размер массива типа string равен: " + surnames.length);
         float[] prices = new float[40];
+        System.out.println("Размер массива типа float равен: " + prices.length);
     }
 }
