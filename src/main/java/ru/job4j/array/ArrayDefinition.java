13f3a2c2d1523d427a7e9ea1794d6f72316f5a5d
TooTopDope <s.cherednichenko.s@gmail.com>
2022-02-20 23:26:47 +0300

    6.0.2. Заполнение массива. [#226300] added 4 names in array.

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива типа string равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива типа float равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Ivan";
+        System.out.println("Имя первого человека: " + names[0]);
+        names[1] = "Alina";
+        System.out.println("Имя второго человека: " + names[1]);
+        names[2] = "Petr";
+        System.out.println("Имя третьего человека: " + names[2]);
+        names[3] = "John";
+        System.out.println("Имя четвертого человека: " + names[3]);
     }
 }
